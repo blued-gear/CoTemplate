674c3d4a8601a57f94c8df36ee35e84de5351020
blued_gear <chocolatecakecodes@disroot.org>
2025-08-06 23:16:50 +0200

    TemplateCompositionTest: update tests

--- src/test/kotlin/apps/chocolatecakecodes/cotemplate/TemplateCompositionTest.kt
+++ src/test/kotlin/apps/chocolatecakecodes/cotemplate/TemplateCompositionTest.kt
@@ -3,16 +3,26 @@ package apps.chocolatecakecodes.cotemplate
 import apps.chocolatecakecodes.cotemplate.TemplateItemTest.Companion.IMG1
 import apps.chocolatecakecodes.cotemplate.TemplateItemTest.Companion.IMG2
 import apps.chocolatecakecodes.cotemplate.db.TemplateEntity
+import apps.chocolatecakecodes.cotemplate.db.TemplateItemEntity
 import apps.chocolatecakecodes.cotemplate.db.UserEntity
+import com.sksamuel.scrimage.ImmutableImage
+import com.sksamuel.scrimage.nio.PngWriter
+import io.kotest.assertions.withClue
 import io.kotest.matchers.shouldBe
 import io.quarkus.test.junit.QuarkusTest
 import io.restassured.module.kotlin.extensions.Then
 import io.restassured.module.kotlin.extensions.When
 import jakarta.enterprise.context.control.ActivateRequestContext
+import jakarta.inject.Inject
 import jakarta.transaction.Transactional
 import org.apache.http.HttpStatus
+import org.eclipse.microprofile.config.inject.ConfigProperty
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import java.nio.file.Files
+import java.nio.file.Path
+import kotlin.random.Random
+import kotlin.random.nextInt
 
 @QuarkusTest
 internal class TemplateCompositionTest {
@@ -22,20 +32,53 @@ internal class TemplateCompositionTest {
         const val EXPECTED_EMPTY = "/images/expected/empty.png"
         const val EXPECTED_BOUNDS = "/images/expected/bounds.png"
         const val EXPECTED_OVERLAY = "/images/expected/overlay.png"
+        const val EXPECTED_ONE = "/images/expected/one.png"
+        const val EXPECTED_PARTIAL_OUTSIDE = "/images/expected/partialOutside.png"
     }
 
-    private fun checkImg(tpl: String, expectedClasspath: String) {
+    @Inject
+    @ConfigProperty(name = "cotemplate.image-storage")
+    lateinit var imgDirPath: String
+
+    private fun checkImg(tpl: String, imgs: List<String>, expectedClasspath: String) {
         When {
-            this.get("/templates/$tpl/template")
+            this.get("/templates/$tpl/template?images=${imgs.joinToString(",")}")
         } Then {
             this.statusCode(HttpStatus.SC_OK)
             this.contentType("image/png")
-            val actual = this.extract().body().asByteArray()
+            val actual = this.extract().body().asByteArray().let {
+                ImmutableImage.loader().fromBytes(it)
+            }
 
             TemplateCompositionTest::class.java.getResourceAsStream(expectedClasspath).use {
-                it.readAllBytes()
+                ImmutableImage.loader().fromStream(it)
             }.let { expected ->
-                actual shouldBe expected
+                try {
+                    actual.width shouldBe expected.width
+                    actual.height shouldBe expected.height
+                    actual.hasAlpha() shouldBe true
+
+                    for(y in (0..<actual.height)) {
+                        for(x in (0..<actual.width)) {
+                            val actualPx = actual.pixel(x, y)
+                            val expectedPx = expected.pixel(x, y)
+
+                            withClue(Pair(x, y)) {
+                                actualPx.alpha() shouldBe expectedPx.alpha()
+                                if(actualPx.alpha() != 0)
+                                    actualPx shouldBe expectedPx
+                            }
+                        }
+                    }
+                } catch(e: AssertionError) {
+                    val name = Random.nextInt(1..1000).toString() + ".png"
+                    val path = Path.of(imgDirPath, "testfail", name)
+                    Files.createDirectories(path.parent)
+                    actual.output(PngWriter.MaxCompression, path)
+                    println("failed image written to $name")
+
+                    throw e
+                }
             }
         }
     }
@@ -44,30 +87,62 @@ internal class TemplateCompositionTest {
     @ActivateRequestContext
     @Transactional
     fun cleanDb() {
-        ;//TODO clear item repo
+        TemplateItemEntity.deleteAll()
         UserEntity.deleteAll()
         TemplateEntity.deleteAll()
+    }
+
+    @BeforeEach
+    @ActivateRequestContext
+    @Transactional
+    fun cleanImages() {
+        Files.list(Path.of(imgDirPath)).forEach {
+            it.toFile().deleteRecursively()
+        }
     }
 
     @Test
     fun emptyTemplate() {
         val tpl = TemplateItemTest.setupTemplate()
-        checkImg(tpl.uniqueName, EXPECTED_EMPTY)
+        checkImg(tpl.uniqueName, emptyList(), EXPECTED_EMPTY)
     }
 
     @Test
     fun bounds() {
         val tpl = TemplateItemTest.setupTemplate()
-        TemplateItemTest.uploadItem(tpl.uniqueName, "", 0, 0, 0, IMG1)
-        TemplateItemTest.uploadItem(tpl.uniqueName, "", 256 - 32, 128 - 32, 0, IMG2)
-        checkImg(tpl.uniqueName, EXPECTED_BOUNDS)
+        val i1 = TemplateItemTest.uploadItem(tpl.uniqueName, "", 0, 0, 0, IMG1)
+        val i2 = TemplateItemTest.uploadItem(tpl.uniqueName, "", 256 - 32, 128 - 32, 0, IMG2)
+        checkImg(tpl.uniqueName, listOf(i1.id, i2.id), EXPECTED_BOUNDS)
     }
 
     @Test
     fun overlay() {
         val tpl = TemplateItemTest.setupTemplate()
-        TemplateItemTest.uploadItem(tpl.uniqueName, "", 33, 0, 1, IMG2)
-        TemplateItemTest.uploadItem(tpl.uniqueName, "", 48, 15, 0, IMG1)
-        checkImg(tpl.uniqueName, EXPECTED_OVERLAY)
+        val i1 = TemplateItemTest.uploadItem(tpl.uniqueName, "", 33, 0, 1, IMG2)
+        val i2 = TemplateItemTest.uploadItem(tpl.uniqueName, "", 48, 15, 0, IMG1)
+        checkImg(tpl.uniqueName, listOf(i1.id, i2.id), EXPECTED_OVERLAY)
+    }
+
+    @Test
+    fun oneItem() {
+        val tpl = TemplateItemTest.setupTemplate()
+        val i1 = TemplateItemTest.uploadItem(tpl.uniqueName, "", 0, 0, 0, IMG1)
+        TemplateItemTest.uploadItem(tpl.uniqueName, "", 256 - 32, 128 - 32, 0, IMG2)
+        checkImg(tpl.uniqueName, listOf(i1.id), EXPECTED_ONE)
+    }
+
+    @Test
+    fun partialOutside() {
+        val tpl = TemplateItemTest.setupTemplate()
+        val i1 = TemplateItemTest.uploadItem(tpl.uniqueName, "", -4, -3, 0, IMG1)
+        val i2 = TemplateItemTest.uploadItem(tpl.uniqueName, "", 229, 102, 0, IMG2)
+        checkImg(tpl.uniqueName, listOf(i1.id, i2.id), EXPECTED_PARTIAL_OUTSIDE)
+    }
+
+    @Test
+    fun outside() {
+        val tpl = TemplateItemTest.setupTemplate()
+        val i1 = TemplateItemTest.uploadItem(tpl.uniqueName, "", -100, -100, 0, IMG1)
+        checkImg(tpl.uniqueName, listOf(i1.id), EXPECTED_EMPTY)
     }
 }
